72f2a13b1ef6701d3021cb8dfcef0fa411b0e69d
Terence30 <terence30360@gmail.com>
2019-12-03 14:30:21 +0100

    Add def welcome_send on model Petowner

--- app/models/petowner.rb
+++ app/models/petowner.rb
@@ -10,4 +10,11 @@ class Petowner < ApplicationRecord
   has_many :petsitters, through: :petsittings
 
   belongs_to :district
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
 end
