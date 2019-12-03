8e603b0e99f7ffd3ad63493af1543badd60bbcf4
Jordi-LG <jordy.leguet@gmail.com>
2019-12-03 20:40:21 +0100

    font-awesome connected to app

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "javascripts")
-
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
