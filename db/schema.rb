b6b8de7027b417d553b52c6d1936b1fa2df53d8a
Jordi-LG <jordy.leguet@gmail.com>
2019-12-03 16:24:03 +0100

    add price to table petsitter + add price to seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2019_12_03_093529) do
     t.string "phone_number"
     t.boolean "garden"
     t.integer "living_space"
+    t.decimal "price"
     t.boolean "account_validate?"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
