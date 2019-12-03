be3020a11734e3bc77ccd0de0cb85fccb9af52e0
Terence30 <terence30360@gmail.com>
2019-12-03 16:37:34 +0100

    Rename method on petsitter_mailer.rb

--- app/mailers/petsitter_mailer.rb
+++ app/mailers/petsitter_mailer.rb
@@ -7,9 +7,16 @@ class PetsitterMailer < ApplicationMailer
     mail(to: @petsitter.email, subject: 'Demande de création en cours') 
   end
 
-  def request_validation(petsitter)
+  def validation_request(petsitter)
     @petsitter = petsitter
     @url  = 'https://ungardienpresdechezsoi.herokuapp.com'
     mail(to: @petsitter.email, subject: 'Validation de votre demande de création')
+  end
+
+  def reservation_request(petowner, petsitter)
+    @petowner = petowner
+    @petsitter = petsitter
+    @url  = 'https://ungardienpresdechezsoi.herokuapp.com' 
+    mail(to: @petowner.email, subject: 'Demande auprès du petsitter envoyé')   
   end
 end
