6c00ddf926386b1d23875a0cd5c53b4ee3c91716
Jordi-LG <jordy.leguet@gmail.com>
2019-12-03 17:33:08 +0100

    Created controller Staticpages + view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'staticpages/team'
+  get 'staticpages/contact'
   devise_for :petsitters
   devise_for :petowners
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
