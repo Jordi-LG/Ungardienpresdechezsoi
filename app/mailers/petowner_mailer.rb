15345e099fedbd712bbc6d3f36ce42b8faecdc34
Terence30 <terence30360@gmail.com>
2019-12-03 15:59:18 +0100

    spelling correction + delete comments + templates request validation

--- app/mailers/petowner_mailer.rb
+++ app/mailers/petowner_mailer.rb
@@ -3,10 +3,7 @@ class PetownerMailer < ApplicationMailer
  
   def welcome_email(petowner)
     @petowner = petowner 
-
     @url  = 'https://ungardienpresdechezsoi.herokuapp.com' 
-
-    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @petowner.email, subject: 'Bienvenue chez nous !') 
   end
 end
