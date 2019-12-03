56f145245d8be08c43271539dd490bc4be0826c7
Paulhennin <henninpaul@gmail.com>
2019-12-03 14:37:23 +0100

    fix controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :petsitters
+  devise_for :petsitters, controllers: {
+    sessions: 'petsitters/sessions'
+  }
   devise_for :petowners
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
