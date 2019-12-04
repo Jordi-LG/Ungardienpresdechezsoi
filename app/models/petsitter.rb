6fc2f781b7812bf90cb152754176e517450fece2
Terence30 <terence30360@gmail.com>
2019-12-04 17:51:34 +0100

    add button edit account petsitters

--- app/models/petsitter.rb
+++ app/models/petsitter.rb
@@ -18,7 +18,7 @@ class Petsitter < ApplicationRecord
   def account_validate_false
   	@petsitter = Petsitter.last
   	@petsitter.account_validate = false
-	@petsitter.save  	
+	  @petsitter.save  	
   end
 
   def demand_creation
