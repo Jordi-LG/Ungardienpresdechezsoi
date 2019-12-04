759e1199db25394757db2a69bd190f9f05ef8240
Jordi-LG <jordy.leguet@gmail.com>
2019-12-04 17:06:31 +0100

    Footer created

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,7 @@ Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "st
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "images")
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "javascripts")
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
