70f5ef824f13ee66daa043102c6131dab6c0e210
Paulhennin <henninpaul@gmail.com>
2019-12-04 12:13:08 +0100

    generate avatar_controller and fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'avatars/create'
   get 'staticpages/team'
   get 'staticpages/contact'
 
   devise_for :petsitters
   devise_for :petowners
-  resources :petsitters, only: [:show, :index]
+  resources :petsitters, only: [:show, :index] do
+    resources :avatars, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :associations, only: [:show]
 end
