9182d56dfe4db0c048ea47b0e0cb56c8d9a4ecca
Paulhennin <henninpaul@gmail.com>
2019-12-04 13:12:00 +0100

    fix issues about uploading pictures

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,6 +1,6 @@
 class AvatarsController < ApplicationController
   def create
-    @petsitter = Petsitter.find(params[:user_id])
+    @petsitter = Petsitter.find(params[:petsitter_id])
 @petsitter.avatar.attach(params[:avatar])
 redirect_to(petsitter_path(@petsitter))
   end
