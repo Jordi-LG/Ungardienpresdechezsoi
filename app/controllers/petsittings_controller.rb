8f5c0d181fa47edc4a63969e675ecdcf1538a4ca
Jordi-LG <jordy.leguet@gmail.com>
2019-12-05 15:38:30 +0100

    [ongoing] view/petsitters/index form_with connection to controller petsitters

--- app/controllers/petsittings_controller.rb
+++ app/controllers/petsittings_controller.rb
@@ -3,6 +3,15 @@ class PetsittingsController < ApplicationController
   end
 
   def create
-    Petsitting.create(petowner_id: current_petowner, petsitter_id: params[:id])
+    @petsitting = Petsittings.new
+    puts "ù"*60
+    @petsitting.petowner_id = current_petowner.id
+    @petsitting.petsitter_id = params[:id]
+    @petsitting.save
+    puts "save"
+
+    if @petsitters.save
+      redirect_to root_path
+    end
   end
 end
