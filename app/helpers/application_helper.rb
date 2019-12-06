091c5ac5f2874bc4cd9f325389f1284950e51a99
Jordi-LG <jordy.leguet@gmail.com>
2019-12-06 10:18:53 +0100

    [create] ApplicationHelpers, exception for show navbar and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 module ApplicationHelper
   include PetsittersHelper
   include PetownersHelper
+
+
+  #HIDE NAVBAR IN SELECTED PAGES
+  def show_navbar
+    !current_page?(controller: 'petowners/registrations', action: 'new') &&
+    !current_page?(controller: 'devise/sessions', action: 'new')
+  end
+
+  def show_footer
+    !current_page?(controller: 'petowners/registrations', action: 'new')
+  end
 end
