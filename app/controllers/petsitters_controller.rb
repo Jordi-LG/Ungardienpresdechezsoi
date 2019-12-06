369e8e871ee92874d772abb0fb3790c00f15777f
Paulhennin <henninpaul@gmail.com>
2019-12-06 13:54:05 +0100

    [upload] restrict for visitor, can't see profil

--- app/controllers/petsitters_controller.rb
+++ app/controllers/petsitters_controller.rb
@@ -1,4 +1,6 @@
 class PetsittersController < ApplicationController
+  before_action :authenticate_petowner! , only: [:show]
+
   def index
     @petsitters = Petsitter.all
   end
@@ -7,5 +9,6 @@ class PetsittersController < ApplicationController
     @petsitter = Petsitter.find(params[:id])
     @bookings = Petsitting.where(petsitter_id: current_petsitter.id)
   end
+
 
 end
