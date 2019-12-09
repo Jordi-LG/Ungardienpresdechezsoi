bb27dea4842b4f1e336d5c9f6e51d9f54e70bf46
meladouz <marine.eladouzi@gmail.com>
2019-12-09 17:51:33 +0100

    [update] Models/pet - add validate when a new pet is created

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,8 @@
 class Pet < ApplicationRecord
   belongs_to :petowner
+
+  validates :name, presence: true
+  validates :pet_type, presence: true
+  validates :sterilized, presence: true
+  validates :sexe, presence: true
 end
