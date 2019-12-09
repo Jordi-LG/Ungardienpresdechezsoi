996f22cb6db22f62aaa16cb5306a8a9e4ccd1648
meladouz <marine.eladouzi@gmail.com>
2019-12-09 19:33:57 +0100

    [update] add avatars to pets

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   resources :associations, only: [:show, :index]
   resources :petsittings, only: [:new, :create, :show]
   resources :pets, only: [:new, :create]
+  
 end
