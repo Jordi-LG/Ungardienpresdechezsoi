d046fd2ef61323666f97d208050ca707c830bc3c
Jordi-LG <jordy.leguet@gmail.com>
2019-12-09 21:12:01 +0100

    [created] Comments / Create controller with #index #create #new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+  get 'comments/create'
+  get 'comments/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'staticpages#index'
