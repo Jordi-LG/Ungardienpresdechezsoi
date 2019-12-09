761ac3933f0bbc9ee752711199139469049acc85
meladouz <marine.eladouzi@gmail.com>
2019-12-09 13:47:47 +0100

    [update] Routes/associations - add index to associations routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :petowners, only: [:show] do
   end
-  resources :associations, only: [:show]
+  resources :associations, only: [:show, :index]
   resources :petsittings, only: [:new, :create, :show]
 end
