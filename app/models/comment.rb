c63f025bd46647b7e6382989c907ca31b2a18cd4
Jordi-LG <jordy.leguet@gmail.com>
2019-12-09 12:58:41 +0100

    [update] table Comments / miss a param for validate in Comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :petowner
 
   validates :title, presence: true
-  validates :content, presence: true, length: { 20.. 250 }
+  validates :content, presence: true, length: { in: 20..250 }
 end
