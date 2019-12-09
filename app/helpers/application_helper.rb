37abb9e8ed867559e52bee5d55a32627c13a4d62
Jordi-LG <jordy.leguet@gmail.com>
2019-12-09 22:26:01 +0100

    [doing] Comments / Create partial for form comments + helpers only if petsitting entry

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   include PetsittersHelper
   include PetownersHelper
+  include CommentsHelper
 
 
   #HIDE NAVBAR IN SELECTED PAGES
@@ -12,15 +13,15 @@ module ApplicationHelper
     !current_page?(controller: 'petsitters/registrations', action: 'new') &&
     !current_page?(controller: 'petsitters/sessions', action: 'new') &&
     !current_page?(controller: 'petsitters/registrations', action: 'edit') &&
-    !current_page?(controller: 'petsitters/passwords', action: 'new') 
+    !current_page?(controller: 'petsitters/passwords', action: 'new')
 
 
   end
 
   def show_footer
     !current_page?(controller: 'petowners/registrations', action: 'new') &&
-    !current_page?(controller: 'petowners/sessions', action: 'new') &&  
-    !current_page?(controller: 'petowners/registrations', action: 'edit') && 
+    !current_page?(controller: 'petowners/sessions', action: 'new') &&
+    !current_page?(controller: 'petowners/registrations', action: 'edit') &&
     !current_page?(controller: 'petowners/passwords', action: 'new') &&
     !current_page?(controller: 'petsitters/registrations', action: 'new') &&
     !current_page?(controller: 'petsitters/sessions', action: 'new')  &&
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,5 @@
 module CommentsHelper
+  def show_comments
+    Petsitting.find_by(petsitter_id: params[:id],petowner_id: current_petowner.id)
+  end
 end
