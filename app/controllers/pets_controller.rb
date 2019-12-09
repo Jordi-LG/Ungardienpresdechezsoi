3dc98660e8db1339240c0bcca20a85885be5f56b
meladouz <marine.eladouzi@gmail.com>
2019-12-09 18:05:19 +0100

    [update] Controllers/pets - add private method for params.require infos from the form

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,22 @@
 class PetsController < ApplicationController
   def new
+    @pet = Pet.index_petowners_on_email
   end
 
   def create
+    @pet = Pet.create(petowner_id: current_petowner.id, name: pet_form_params['name'], birthday: pet_form_params['birthday'], pet_type: pet_form_params['pet_type'], sterilized: pet_form_params['sterilized'], sexe: pet_form_params['sexe'])
+    if @pet.save
+      flash[:alert] = "Le profil de votre animal a bien été enregistré !"
+      redirect_to petowner_path(id: current_petowner.id)
+    else
+      flash[:warning] = @pet.errors.messages
+    end
   end
+
+  private
+
+  def pet_form_params
+    params.require(:pet).permit(:name, :birthday, :pet_type, :sterilized, :sexe)
+  end
+
 end
