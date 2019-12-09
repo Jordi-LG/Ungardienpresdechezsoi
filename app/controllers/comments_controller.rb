88b2180e25843442850f3a2de4b7822c42b41200
Jordi-LG <jordy.leguet@gmail.com>
2019-12-09 23:29:23 +0100

    [doing] Comments / Front done for index comments + back entry DB for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,29 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.all
   end
 
   def create
+    @comment = Comment.new(comments_form)
+    @comment.ps_id = params[:id]
+    @comment.petowner_id = current_petowner.id
+    @comment.save
+
+      if @comment.save
+        redirect_to petsitter_path(page_id)
+      end
   end
 
   def index
+  end
+
+  private
+
+  def comments_form
+    params.permit(:title, :content)
+  end
+
+  def page_id
+    params.require(:id)
   end
 end
