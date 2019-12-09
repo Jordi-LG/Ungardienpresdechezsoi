7d97f91af1b5d794a318d49e53ed84765463de28
Jordi-LG <jordy.leguet@gmail.com>
2019-12-09 12:13:43 +0100

    [doing] table Comments / Add entries to migrate

--- db/migrate/20191209110602_create_comments.rb
+++ db/migrate/20191209110602_create_comments.rb
@@ -1,7 +1,10 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-
+      t.string :title
+      t.text :content
+      t.integer :ps_id
+      
       t.timestamps
     end
   end
