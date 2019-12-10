bb34bad88fe611d1c6c9cfc5b4b8eab5e7c8edb9
Terence30 <terence30360@gmail.com>
2019-12-10 19:17:10 +0100

    Add validate on dashboard admin

--- app/admin/associations.rb
+++ app/admin/associations.rb
@@ -5,14 +5,27 @@ ActiveAdmin.register Association do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :describe, :date_of_birth, :email, :facebook_url, :donation_url
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :describe, :date_of_birth, :email, :facebook_url, :donation_url]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :name, :describe, :date_of_birth, :email, :facebook_url, :donation_url, :validate_association
+
+  action_item :validate, only: :show do
+    link_to "Validate", validate_admin_association_path(association), method: :put if association.validate_association == false
+  end
+
+  member_action :validate, method: :put do
+    association = Association.find(params[:id])
+    association.update(validate_association: true)
+    #PetsitterMailer.validation_request(petsitter).deliver_now
+    redirect_to admin_root_path
+  end
+
+    action_item :unvalidate, only: :show do
+    link_to "Unvalidate", unvalidate_admin_association_path(association), method: :put if association.validate_association == true
+  end
+
+  member_action :unvalidate, method: :put do
+    association = Association.find(params[:id])
+    association.update(validate_association: false)
+    #PetsitterMailer.validation_request(petsitter).deliver_now
+    redirect_to admin_root_path
+  end
 end
