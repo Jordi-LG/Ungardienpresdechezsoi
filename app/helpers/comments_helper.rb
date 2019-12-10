e2ffa4573ae38f978a9e3ec69a899e902096b6c9
Jordi-LG <jordy.leguet@gmail.com>
2019-12-10 22:27:01 +0100

    [Done] Comments index / Index done + dropdown search + total comment by petsitter

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,8 @@ module CommentsHelper
   def show_comments
     Petsitting.find_by(petsitter_id: params[:id],petowner_id: current_petowner.id)
   end
+
+  def comments_petsitter(id)
+    Comment.where(ps_id: id)
+  end
 end
