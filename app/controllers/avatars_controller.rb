e1951f03b9a00308dbdf90f748792fbc8d33e240
Terence30 <terence30360@gmail.com>
2019-12-11 23:46:42 +0100

    [fix] controller avatar error when avatar is nil

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -3,21 +3,39 @@ class AvatarsController < ApplicationController
 
     if current_petsitter
       @petsitter = Petsitter.find(params[:petsitter_id])
-      @petsitter.avatar.attach(params[:avatar])
-      redirect_to(petsitter_path(@petsitter))
+      if params[:avatar] == nil
+        flash[:alert] = "Votre photo n'a pas été ajouté"
+        redirect_to petsitter_path(current_petsitter.id)
+      else 
+        @petsitter.avatar.attach(params[:avatar])
+        flash[:success] = 'Votre photo a été ajouté'
+        redirect_to(petsitter_path(@petsitter))
+      end
+    
     else
       if params[:pet_id]== nil
         @petowner = Petowner.find(current_petowner.id)
-        @petowner.avatar.attach(params[:avatar])
+        if params[:avatar] == nil
+          flash[:alert] = "Votre photo n'a pas été ajouté"
+          redirect_to petowner_path(current_petowner.id)
+
+        else 
+          @petowner.avatar.attach(params[:avatar])
+          flash[:success] = 'Votre photo a été ajouté'
+          redirect_to petowner_path(current_petowner.id)
+        end
+
       else
-        @pet = Pet.find(params[:pet_id])
-        @pet.avatar.attach(params[:avatar])
-
+        if params[:avatar] == nil
+          flash[:alert] = "Votre photo n'a pas été ajouté"
+          redirect_to petowner_path(current_petowner.id)
+        else
+          @pet = Pet.find(params[:pet_id])
+          @pet.avatar.attach(params[:avatar])
+          flash[:success] = 'Votre photo a été ajouté'
+          redirect_to petowner_path(current_petowner.id)
+        end
       end
-      redirect_to(petowner_path(current_petowner))
     end
-
   end
-
-
 end
