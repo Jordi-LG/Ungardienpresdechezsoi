6b5e81f33175f7ca4332be694e6203d039a7b1fa
Paulhennin <henninpaul@gmail.com>
2019-12-11 15:06:32 +0100

    [update] Method destroy for petsitting & button in 'reservation'

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   end
 
   resources :associations, only: [:show, :index, :new, :create]
-  resources :petsittings, only: [:new, :create, :show, :update]
+  resources :petsittings, only: [:new, :create, :show, :update, :destroy]
   resources :contacts, only: [:new, :create]
   resources :comments, only: [:new, :create, :index]
 
