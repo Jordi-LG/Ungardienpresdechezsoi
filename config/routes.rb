da1a8a7d72ff54d415951dd3c117db6ecf32baa7
Jordi-LG <jordy.leguet@gmail.com>
2019-12-13 11:53:56 +0100

    [update] Routes / Change names URL for petowners

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     resources :avatars, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :petowners, only: [:show], path: 'monprofil' do
+  resources :petowners, only: [:show], path: 'profil' do
     resources :avatars, only: [:create]
   end
 
