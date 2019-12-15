25f81efe7cd00b5d80d30a7a02179fae70a21a1e
Terence30 <terence30360@gmail.com>
2019-12-16 00:48:14 +0100

    add validate to first-name and last name lenght minimum 2

--- app/models/petowner.rb
+++ app/models/petowner.rb
@@ -16,8 +16,8 @@ class Petowner < ApplicationRecord
 
   after_create :welcome_send
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, presence: true, length: { minimum: 3 }
+  validates :last_name, presence: true, length: { minimum: 3 }
   validates :describe, presence: true, length: { in: 20..1000 }
   validates :age, presence: true
   validates :phone_number, presence: true, format: { with: /\A(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})\z/, message: "Entrer un numero de telephone valide" }
